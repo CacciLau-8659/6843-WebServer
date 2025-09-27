7f03ce98101bb33ebecccb7996230388db58e74a
Cacci Lau <cl8659@nyu.edu>
2025-09-27 17:54:05 -0400

    Add files via upload

--- webServer.py
+++ webServer.py
@@ -16,46 +16,46 @@ def webServer(port=13331):
         print('Ready to serve...')
         connectionSocket, addr = serverSocket.accept() 
     
-    try:
+        try:
         
-        message = connectionSocket.recv(1024).decode()
-        filename = message.split()[1]
+            message = connectionSocket.recv(1024).decode()
+            filename = message.split()[1]
 
-        #opens the client requested file. 
-        f = open(filename[1:], "rb")
+            #opens the client requested file. 
+            f = open(filename[1:], "rb")
 
-        #This variable can store the headers you want to send for any valid or invalid request.   What header should be sent for a response that is ok?    
-        header = b"HTTP/1.1 200 OK\r\n"
-        header += b"Server: 6843-PythonServer\r\n"
-        header += b"Content-Type: text/html; charset=UTF-8\r\n"
-        header += b"Connection: close \r\n\r\n"
-        #Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
+            #This variable can store the headers you want to send for any valid or invalid request.   What header should be sent for a response that is ok?    
+            output_data = b"HTTP/1.1 200 OK\r\n"
+            output_data += b"Server: 6843-PythonServer\r\n"
+            output_data += b"Content-Type: text/html; charset=UTF-8\r\n"
+            output_data += b"Connection: close \r\n\r\n"
+            #Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
 
-        for i in f:
-            header += i
+            for i in f:
+                output_data += i
         
-        #Send the content of the requested file to the client (don't forget the headers you created)!
-        #Send everything as one send command, do not send one line/item at a time!
+            #Send the content of the requested file to the client (don't forget the headers you created)!
+            #Send everything as one send command, do not send one line/item at a time!
 
-        connectionSocket.send(header)
-        connectionSocket.close() #closing the connection socket
+            connectionSocket.send(output_data)
+            connectionSocket.close() #closing the connection socket
 
-    except Exception as e:
-        # Send response message for invalid request due to the file not being found (404)
-        # Remember the format you used in the try: block!
-        header = b"HTTP/1.1 404 Not Found\r\n"
-        header += b"Server: 6843-PythonServer\r\n"
-        header += b"Content-Type: text/html; charset=UTF-8\r\n"
-        header += b"Connection: close \r\n\r\n"
-        header = b"<html><body><h1> 404 Not Found </h1><body></html>"
+        except Exception as e:
+            # Send response message for invalid request due to the file not being found (404)
+            # Remember the format you used in the try: block!
+            output_data = b"HTTP/1.1 404 Not Found\r\n"
+            output_data += b"Server: 6843-PythonServer\r\n"
+            output_data += b"Content-Type: text/html; charset=UTF-8\r\n"
+            output_data += b"Connection: close \r\n\r\n"
+            output_data = b"<html><body><h1> 404 Not Found </h1><body></html>"
         
-        #Close client socket
-        connectionSocket.close()
+            #Close client socket
+            connectionSocket.close()
 
-        # Commenting out the below (some use it for local testing). It is not required for Grade scope, and some students have moved it erroneously in the While loop. 
-        # DO NOT PLACE ANYWHERE ELSE AND DO NOT UNCOMMENT WHEN SUBMITTING, YOU ARE GONNA HAVE A BAD TIME
-        #serverSocket.close()
-        #sys.exit()  # Terminate the program after sending the corresponding data
+            # Commenting out the below (some use it for local testing). It is not required for Grade scope, and some students have moved it erroneously in the While loop. 
+            # DO NOT PLACE ANYWHERE ELSE AND DO NOT UNCOMMENT WHEN SUBMITTING, YOU ARE GONNA HAVE A BAD TIME
+            #serverSocket.close()
+            #sys.exit()  # Terminate the program after sending the corresponding data
 
 if __name__ == "__main__":
     webServer(13331)
